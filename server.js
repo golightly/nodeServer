de51285009613a52ed407ef78d5164923121c334
golightly <golightlydev@gmail.com>
2019-09-22 15:41:08 -0400

    dis/re connect

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
         console.log("A USER DISCONNECTED!");
         console.log("message1: " + message.msg1);
         console.log("message2: " + message.msg2);
-        socket.disconnect();
+        //socket.disconnect();
     });
 });
 
