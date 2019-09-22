bf4c66068556a472b860ec860743c35756fcba1d
golightly <golightlydev@gmail.com>
2019-09-22 18:16:13 -0400

    disconnect test

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ io.on('connection', () => {
 io.on('connection', (socket) => {
     console.log("test message");
     socket.on('disconnectMessage', (message) => {
+        socket.emit("SHOULD BE RECEIVED BY CURRENT USER ONLY!");
         console.log("message1: " + message.msg1);
         console.log("message2: " + message.msg2);
         io.emit('serverResponse', {msg: "test message from server"});
